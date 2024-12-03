3bc575761332560633fc5345a9dfc9b8036569dd
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-12-03 22:11:38 +0530

    Trying a couple of ways to parse the markdown - 1. using mistune library via HTML - 2. using vault.get_source_text() method of obsidiantools via TEXT

--- src/note_utils.py
+++ src/note_utils.py
@@ -1,4 +1,29 @@
-def parse_note(note, vault):
+from mistune.plugins.task_lists import task_lists
+import mistune
+import pypandoc
+import json
+
+markdown = mistune.create_markdown(plugins=['task_lists'])
+
+
+def parse_note_via_html(note, vault):
     note = '2024 Oct'
-    text = vault.get_source_text(note)
+    note_path = VAULT_LOC / vault.md_file_index[note]
+    with open(note_path, 'r', encoding="utf8") as f:
+        text = f.read()
+    tasks = markdown(text)
+
+
+def parse_note_via_json(note, vault):
+    note = '2024 Oct'
+    note_path = VAULT_LOC / vault.md_file_index[note]
+    text_dict = json.loads(pypandoc.convert_file(note_path, to='json'))
+    lines = [line for line in text_dict['blocks'] if line.strip()]
+    tasks = task_lists(text)
+
+
+def parse_note_via_text(note, vault):
+    note = '2024 Oct'
+    note_path = VAULT_LOC / vault.md_file_index[note]
+    text = vault.get_source_text(note)  # Doesn't identify bullet points as separate lines
     lines = [line for line in text.split('\n') if line.strip()]
