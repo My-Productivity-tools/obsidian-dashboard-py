c6bb2daac083e1d2bc5f17e0a6e6a25a967505a4
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-12-04 19:07:32 +0530

    1. parse_note_via_html() function - added code to convert html to soup and then to tasks 2. Added parse_html_for_tasks() function to extract a tree of task objects from HTML 3. Added a stub for convert_to_task() function to create Task object from a HTML element

--- src/note_utils.py
+++ src/note_utils.py
@@ -2,6 +2,8 @@ from mistune.plugins.task_lists import task_lists
 import mistune
 import pypandoc
 import json
+from bs4 import BeautifulSoup
+from itertools import chain
 
 markdown = mistune.create_markdown(plugins=['task_lists'])
 
@@ -11,15 +13,44 @@ def parse_note_via_html(note, vault):
     note_path = VAULT_LOC / vault.md_file_index[note]
     with open(note_path, 'r', encoding="utf8") as f:
         text = f.read()
-    tasks = markdown(text)
+    html = markdown(text)
+    soup = BeautifulSoup(html, 'html.parser')
+    return parse_html_for_tasks(soup)
+
+
+def parse_html_for_tasks(elem):
+    """
+    Recursively filters the element tree to retain only the required tasks 
+    while retaining the tree structure.
+
+    :param elem: A HTML element tree that needs to be parsed for tasks.
+    :return: A new HTML element tree with only the required tasks.
+    """
+    # Filter the children recursively
+    filtered_children = list(chain.from_iterable(parse_html_for_tasks(child) 
+                                                 for child in elem.children))
+
+    # If the current node is a required task type, include it
+    if elem.name == "li" and 'task-list-item' in elem.attrs['class']:
+        return convert_to_task(elem, filtered_children)
+    else:
+        return filtered_children
+
+
+def convert_to_task(elem, children=[]):
+    """
+    Converts a HTML element into a task object.
+
+    :param elem: A HTML element to be converted into a task.
+    :return: A task object.
+    """
+    pass
 
 
 def parse_note_via_json(note, vault):
     note = '2024 Oct'
     note_path = VAULT_LOC / vault.md_file_index[note]
     text_dict = json.loads(pypandoc.convert_file(note_path, to='json'))
-    lines = [line for line in text_dict['blocks'] if line.strip()]
-    tasks = task_lists(text)
 
 
 def parse_note_via_text(note, vault):
