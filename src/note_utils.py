02531dcbec9057027b56a2bfb2a61a9f3143677d
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-12-09 11:54:17 +0530

    1. Fixed a bug where Markdown comments were spoiling the tasks extraction; 2. Autoformatting

--- src/note_utils.py
+++ src/note_utils.py
@@ -39,14 +39,18 @@ def parse_html_for_tasks(elem, task_tree, root, note, okr=None):
             if 'okr' in task.data and task.data['okr'] == okr:
                 task_tree.add_node(task, root)
                 # Children should not be checked for OKR if the parent is marked for the OKR
-                [parse_html_for_tasks(child, task_tree, task.identifier, note) for child in children]
+                [parse_html_for_tasks(
+                    child, task_tree, task.identifier, note) for child in children]
             else:
-                [parse_html_for_tasks(child, task_tree, root, note, okr) for child in children]
+                [parse_html_for_tasks(child, task_tree, root, note, okr)
+                 for child in children]
         else:
-                task_tree.add_node(task, root)
-                [parse_html_for_tasks(child, task_tree, task.identifier, note, okr) for child in children]
+            task_tree.add_node(task, root)
+            [parse_html_for_tasks(
+                child, task_tree, task.identifier, note, okr) for child in children]
     else:
-        [parse_html_for_tasks(child, task_tree, root, note, okr) for child in children]
+        [parse_html_for_tasks(child, task_tree, root, note, okr)
+         for child in children]
 
 
 def convert_to_task(elem, note):
@@ -59,49 +63,52 @@ def convert_to_task(elem, note):
     task_node = Node()
     task = {}
     task['raw_text'] = elem.text  # storing raw text
-        
+
     for child in elem.find_all():
         if child.name not in ['a', 'span', 'strong']:
             child.decompose()
     text = elem.get_text()
+    comment_pattern = r'%%(.+)[%%]?'
+    text = re.sub(comment_pattern, '', text, flags=re.DOTALL)
     task['title'] = text[3:].strip()
     status_map = {
         '[ ]': 'Todo',
         '[x]': 'Done',
         '[/]': 'In Progress',
-        '[-]': 'Cancelled', 
-        '[|]': 'Blocked', 
+        '[-]': 'Cancelled',
+        '[|]': 'Blocked',
     }
     priority_map = {  # Normal priority is when no symbol is specified
-        '\\u23ec': 'Lowest', 
-        '\\ud83d\\udd3d': 'Low', 
-        '\\ud83d\\udd3c': 'Medium', 
-        '\\u23eb': 'High', 
+        '\\u23ec': 'Lowest',
+        '\\ud83d\\udd3d': 'Low',
+        '\\ud83d\\udd3c': 'Medium',
+        '\\u23eb': 'High',
         '\\ud83d\\udd3a': 'Highest'
     }
     dates_map = {
-        '\\u2795': 'Created Date', 
-        '\\ud83d\\udeeb': 'Started Date', 
-        '\\u23f3': 'Scheduled Date', 
-        '\\ud83d\\udcc5': 'Due Date', 
-        '\\u2705': 'Done Date', 
+        '\\u2795': 'Created Date',
+        '\\ud83d\\udeeb': 'Started Date',
+        '\\u23f3': 'Scheduled Date',
+        '\\ud83d\\udcc5': 'Due Date',
+        '\\u2705': 'Done Date',
         '\\u274c': 'Cancelled Date'
     }
     task['status'] = status_map.get(text[:3], None)
 
-    task['tags'] = [word.strip('#') for word in task['title'].split() 
+    task['tags'] = [word.strip('#') for word in task['title'].split()
                     if word.startswith('#')]
     title_words = json.dumps(task['title']).strip('"').split()
     task['fields'] = [word for word in title_words if word.startswith('\\u')]
-    
-    priority = [priority_map.get(field) for field in task['fields'] 
+
+    priority = [priority_map.get(field) for field in task['fields']
                 if field in priority_map]
     task['priority'] = priority[0] if priority else None
 
     date_fields_utf = [field for field in task['fields'] if field in dates_map]
     for date_field in date_fields_utf:
-        task[dates_map.get(date_field)] = title_words[title_words.index(date_field)+1]
-    
+        task[dates_map.get(date_field)
+             ] = title_words[title_words.index(date_field)+1]
+
     pattern_okr = r'\(([a-zA-Z\s]+)::(.+)\)'
     matches_okr = re.findall(pattern_okr, task['title'])
     for match in matches_okr:
@@ -116,7 +123,8 @@ def convert_to_task(elem, note):
             val = float(val)
         task[key] = val
 
-    task_types = [tag for tag in task['tags'] if tag in ['epic', 'story', 'task']]
+    task_types = [tag for tag in task['tags']
+                  if tag in ['epic', 'story', 'task']]
     if len(task_types) == 1:
         task['type'] = task_types[0]
     elif len(task_types) == 0:
@@ -124,7 +132,7 @@ def convert_to_task(elem, note):
     else:
         print(elem)
         raise ValueError(f"Multiple task types found: {task_types}")
-    
+
     task['file_name'] = note
     # TODO: Add additional fields if required - description w/o tags & field tags
 
