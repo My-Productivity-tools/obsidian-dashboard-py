c4beae63de21e31bebb5a9a2ac697938019124b2
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-12-09 12:04:12 +0530

    1. Fixed a bug where nonexistent notes are causing issue in extracting tasks; 2. Minor rework and autoformatting;

--- src/utils.py
+++ src/utils.py
@@ -35,8 +35,9 @@ def parse_okr_note(okr_note, vault):
     # Get the Objectives
     obj_pattern = r'(O\d+):(.+)'
     obj_matches = [re.search(obj_pattern, e.text)
-                 for e in soup.findAll('h1', recursive=False)]
-    okr_info = {m[1].strip(): {'name': m[2].strip(), 'kr_info': {}} for m in obj_matches}
+                   for e in soup.findAll('h1', recursive=False)]
+    okr_info = {m[1].strip(): {'name': m[2].strip(), 'kr_info': {}}
+                for m in obj_matches}
 
     # Get the Key Results
     kr_pattern = r'(O\d+)\s(KR\d+):(.+)'
@@ -53,23 +54,27 @@ def parse_okr_note(okr_note, vault):
 
 def get_kr_data(okr_info, vault):
     okr_data = okr_info.copy()
-    note = 'Deep Learning'
-    note_path = VAULT_LOC / vault.md_file_index[note]
+    # note = 'Deep Learning'
+    # note_path = VAULT_LOC / vault.md_file_index[note]
+    note_metadata = vault.get_note_metadata()
     # TODO: Parse all relevant notes for tags marked w/ the OKR tag
-    for obj in okr_info.keys():
+    for obj in okr_data.keys():
         print(obj)
-        for kr in okr_info[obj]['kr_info'].keys():
+        for kr in okr_data[obj]['kr_info'].keys():
             print(kr)
             tasks = Tree()
             tasks.create_node("Master Root", 'master_root')
             for note in vault.md_file_index.keys():
                 print(note)
-                note_tasks = \
-                    parse_note_for_tasks(note, vault, okr_info[obj]['kr_info'][kr]['okr_tag'])
-                tasks.paste(note_tasks, 'master_root').link_past_node('root')
-            okr_info[obj]['kr_info'][kr]['kr_data'] = tasks
+                if note_metadata.loc[note_metadata.index == note, 'note_exists'].iloc[0]:
+                    note_tasks = \
+                        parse_note_for_tasks(
+                            note, vault, okr_data[obj]['kr_info'][kr]['okr_tag'])
+                    tasks.paste('master_root', note_tasks)
+                    tasks.link_past_node('root')
+            okr_data[obj]['kr_info'][kr]['kr_data'] = tasks
     return okr_data
-                
+
     # TODO: Parse all Daily notes for all kinds of tasks / events
     # TODO: Extract duration from all tasks & events
 
